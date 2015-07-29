631c2d1ff8394e9b1988151dbd2d2d7ab318086d
aevitas <m4rtijn@gmail.com>
2015-07-29 11:28:56 +0200

    Added missing AssemblyInfo

---
+++ trunk/DefaultCombat/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BuddyWing.DefaultCombat")]
+[assembly: AssemblyDescription("Default combat routine for BuddyWing, the bot for SWTOR")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bossland GmbH")]
+[assembly: AssemblyProduct("DefaultCombat.Properties")]
+[assembly: AssemblyCopyright("Copyright © Chipsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f1c3516d-4c45-412a-89ec-e69313cde767")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1")]
+[assembly: AssemblyFileVersion("1")]
