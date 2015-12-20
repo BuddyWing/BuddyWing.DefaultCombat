6fc2523f39a1a289fc0055304119df66aed039bf
alltrueist <dj_owen@hotmail.com>
2015-12-20 09:28:33 -0500

    Madness fixes

--- trunk/DefaultCombat/Routines/Advanced/Sorcerer/Madness.cs
+++ trunk/DefaultCombat/Routines/Advanced/Sorcerer/Madness.cs
@@ -1,77 +1,82 @@
-﻿// Copyright (C) 2011-2015 Bossland GmbH
-// See the file LICENSE for the source code's detailed license
+﻿// Copyright (C) 2011-2015 Bossland GmbH// See the file LICENSE for the source code's detailed license
+
 
 using Buddy.BehaviorTree;
 using DefaultCombat.Core;
 using DefaultCombat.Helpers;
 
+
 namespace DefaultCombat.Routines
 {
-	internal class Madness : RotationBase
-	{
-		public override string Name
-		{
-			get { return "Sorcerer Madness"; }
-		}
+    internal class Madness : RotationBase
+    {
+        public override string Name
+        {
+            get { return "Sorcerer Madness"; }
+        }
 
-		public override Composite Buffs
-		{
-			get
-			{
-				return new PrioritySelector(
-					Spell.Buff("Mark of Power")
-					);
-			}
-		}
 
-		public override Composite Cooldowns
-		{
-			get
-			{
-				return new LockSelector(
-					Spell.Buff("Force Barrier", ret => Me.HealthPercent <= 20),
-					Spell.Buff("Unnatural Preservation", ret => Me.HealthPercent <= 70),
-					Spell.Buff("Cloud Mind"),
-					Spell.Buff("Polarity Shift"),
-					Spell.Buff("Recklessness")
-					);
-			}
-		}
+        public override Composite Buffs
+        {
+            get
+            {
+                return new PrioritySelector(
+                    Spell.Buff("Mark of Power")
+                    );
+            }
+        }
 
-		public override Composite SingleTarget
-		{
-			get
-			{
-				return new LockSelector(
-					//Movement
-					CombatMovement.CloseDistance(Distance.Ranged),
 
-					//Rotation
-					Spell.Cast("Jolt", ret => Me.CurrentTarget.IsCasting && !DefaultCombat.MovementDisabled),
-					Spell.Cast("Electrocute", ret => Me.CurrentTarget.IsCasting && !DefaultCombat.MovementDisabled),
-					Spell.CastOnGround("Death Field"),
-					Spell.Cast("Crushing Darkness", ret => Me.HasBuff("Wrath")),
-					Spell.Cast("Affliction",
-						ret =>
-							!Me.CurrentTarget.HasDebuff("Affliction (Force)") || Me.CurrentTarget.DebuffTimeLeft("Affliction (Force)") <= 3),
-					Spell.DoT("Creeping Terror", "", 15000),
-					Spell.Cast("Creeping Terror"),
-					Spell.Cast("Force Lightning"),
-					Spell.Cast("Shock"));
-			}
-		}
+        public override Composite Cooldowns
+        {
+            get
+            {
+                return new PrioritySelector(
+                    Spell.Buff("Force Barrier", ret => Me.HealthPercent <= 20),
+                    Spell.Buff("Static Barrier", ret => !Me.HasBuff("Static Barrier") && !Me.HasDebuff("Deionized")),
+                    Spell.Buff("Unnatural Preservation", ret => Me.HealthPercent <= 50),
+                    Spell.Buff("Polarity Shift", ret => Me.CurrentTarget.BossOrGreater()),
+                    Spell.Buff("Recklessness")
+                    );
+            }
+        }
 
-		public override Composite AreaOfEffect
-		{
-			get
-			{
-				return new Decorator(ret => Targeting.ShouldAoe,
-					new LockSelector(
-						Spell.CastOnGround("Death Field"),
-						Spell.DoT("Affliction", "Affliction (Force)"),
-						Spell.CastOnGround("Force Storm")
-						));
-			}
-		}
-	}
+
+        public override Composite SingleTarget
+        {
+            get
+            {
+                return new PrioritySelector(
+                    //Movement
+                    CombatMovement.CloseDistance(Distance.Ranged),
+                    Spell.Cast("Jolt", ret => Me.CurrentTarget.IsCasting && !DefaultCombat.MovementDisabled),
+                    Spell.Cast("Electrocute", ret => Me.CurrentTarget.IsCasting && !DefaultCombat.MovementDisabled),
+                    Spell.Cast("Demolish", ret => Me.BuffCount("Wrath") == 4),
+                    Spell.DoT("Affliction", "Affliction"),
+                    Spell.DoT("Creeping Terror", "Creeping Terror"),
+                    Spell.CastOnGround("Death Field",
+                        ret => Me.CurrentTarget.HasDebuff("Affliction") && Me.CurrentTarget.HasDebuff("Creeping Terror")),
+                    Spell.Cast("Force Leach", ret => Me.CurrentTarget.HasDebuff("Affliction")),
+                    Spell.Cast("Lightning Strike", ret => Me.BuffCount("Wrath") == 4),
+                    Spell.Cast("Force Lightning", ret => Me.BuffCount("Wrath") < 4)
+                    );
+            }
+        }
+
+
+        public override Composite AreaOfEffect
+        {
+            get
+            {
+                return new Decorator(ret => Targeting.ShouldAoe,
+                    new PrioritySelector(
+                        Spell.DoT("Affliction", "Affliction"),
+                        Spell.DoT("Creeping Terror", "Creeping Terror"),
+                        Spell.CastOnGround("Death Field",
+                            ret => Me.CurrentTarget.HasDebuff("Affliction") && Me.CurrentTarget.HasDebuff("Creeping Terror")),
+                        Spell.CastOnGround("Force Storm", ret => Me.CurrentTarget.HasDebuff("Overwhelmed (Mental)"))
+                        ));
+            }
+        }
+    }
 }
