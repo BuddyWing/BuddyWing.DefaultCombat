bcd586ea4e2d3441237b4784cff58bb1529ec02f
alltrueist <dj_owen@hotmail.com>
2015-12-31 19:40:52 -0500

    Gunslinger

--- trunk/DefaultCombat/Routines/Advanced/Gunslinger/Saboteur.cs
+++ trunk/DefaultCombat/Routines/Advanced/Gunslinger/Saboteur.cs
@@ -1,81 +1,93 @@
-﻿// Copyright (C) 2011-2015 Bossland GmbH
-// See the file LICENSE for the source code's detailed license
+﻿// Copyright (C) 2011-2015 Bossland GmbH// See the file LICENSE for the source code's detailed license
+
 
 using Buddy.BehaviorTree;
 using DefaultCombat.Core;
 using DefaultCombat.Helpers;
 
+
 namespace DefaultCombat.Routines
 {
-	internal class Saboteur : RotationBase
-	{
-		public override string Name
-		{
-			get { return "Gunslinger Saboteur"; }
-		}
+    internal class Saboteur : RotationBase
+    {
+        public override string Name
+        {
+            get { return "Gunslinger Saboteur"; }
+        }
 
-		public override Composite Buffs
-		{
-			get
-			{
-				return new PrioritySelector(
-					Spell.Buff("Lucky Shots")
-					);
-			}
-		}
 
-		public override Composite Cooldowns
-		{
-			get
-			{
-				return new LockSelector(
-					Spell.Buff("Escape"),
-					Spell.Buff("Defense Screen", ret => Me.HealthPercent <= 50),
-					Spell.Buff("Dodge", ret => Me.HealthPercent <= 30),
-					Spell.Buff("Cool Head", ret => Me.EnergyPercent <= 50),
-					Spell.Buff("Smuggler's Luck"),
-					Spell.Buff("Illegal Mods")
-					);
-			}
-		}
+        public override Composite Buffs
+        {
+            get
+            {
+                return new PrioritySelector(
+                    Spell.Buff("Lucky Shots")
+                    );
+            }
+        }
 
-		public override Composite SingleTarget
-		{
-			get
-			{
-				return new LockSelector(
-					Spell.Cast("Flurry of Bolts", ret => Me.EnergyPercent < 60),
 
-					//Movement
-					CombatMovement.CloseDistance(Distance.Ranged),
+        public override Composite Cooldowns
+        {
+            get
+            {
+                return new PrioritySelector(
+                    Spell.Buff("Escape"),
+                    Spell.Buff("Defense Screen", ret => Me.HealthPercent <= 50),
+                    Spell.Buff("Dodge", ret => Me.HealthPercent <= 30),
+                    Spell.Buff("Cool Head", ret => Me.EnergyPercent <= 50),
+                    Spell.Buff("Smuggler's Luck"),
+                    Spell.Buff("Illegal Mods")
+                    );
+            }
+        }
 
-					//Rotation
-					Spell.Cast("Crouch", ret => !Me.IsInCover()),
-					Spell.Cast("Flourish Shot", ret => !Me.CurrentTarget.HasDebuff("Armor Reduced")),
-					Spell.Cast("Speed Shot", ret => Me.IsInCover()),
-					Spell.Cast("Incendiary Grenade"),
-					Spell.DoT("Shock Charge", "Shock Charg"),
-					Spell.DoT("Vital Shot", "Bleeding (Vital Shot)"),
-					Spell.Cast("Sabotage Charge", ret => Me.IsInCover()),
-					Spell.Cast("Quickdraw", ret => Me.CurrentTarget.HealthPercent <= 30),
-					Spell.Cast("Aimed Shot", ret => Me.IsInCover()),
-					Spell.Cast("Charged Burst", ret => Me.IsInCover()),
-					Spell.Cast("Quick Shot", ret => !Me.IsInCover())
-					);
-			}
-		}
 
-		public override Composite AreaOfEffect
-		{
-			get
-			{
-				return new Decorator(ret => Targeting.ShouldAoe,
-					new LockSelector(
-						Spell.CastOnGround("XS Freighter Flyby"),
-						Spell.Cast("Incendiary Grenade"),
-						Spell.Cast("Thermal Grenade")
-						));
-			}
-		}
-	}
+        public override Composite SingleTarget
+        {
+            get
+            {
+                return new PrioritySelector(
+                    //Low Energy
+                    new Decorator(ret => Me.EnergyPercent < 60,
+                        new PrioritySelector(
+                        Spell.Cast("Thermal Grenade", ret => Me.HasBuff("Seize the Moment")),
+                        Spell.Cast("Rifle Shot")
+                        )),
+
+
+                    //Movement
+                    CombatMovement.CloseDistance(Distance.Ranged),
+
+
+                    //Rotation
+                    Spell.Cast("Distraction", ret => Me.CurrentTarget.IsCasting && !DefaultCombat.MovementDisabled),
+                    Spell.Cast("Sabotage Charge"),
+                    Spell.DoTGround("Incendiary Grenade", 9000),
+                    Spell.Cast("Speed Shot"),
+                    Spell.DoT("Shock Charge", "Shock Charge"),
+                    Spell.Cast("Sabotage", ret => Me.CurrentTarget.HasDebuff("Shock Charge")),
+                    Spell.Cast("Thermal Grenade", ret => Me.HasBuff("Seize the Moment")),
+                    Spell.CastOnGround("XS Freighter Flyby", ret => Me.EnergyPercent > 75),
+                    Spell.DoT("Vital Shot", "Vital Shot"),
+                    Spell.Cast("Quickdraw", ret => Me.CurrentTarget.HealthPercent <= 30),
+                    Spell.Cast("Rifle Shot")
+                    );
+            }
+        }
+
+
+        public override Composite AreaOfEffect
+        {
+            get
+            {
+                return new Decorator(ret => Targeting.ShouldAoe,
+                    new PrioritySelector(
+                        Spell.CastOnGround("XS Freighter Flyby"),
+                        Spell.Cast("Incendiary Grenade"),
+                        Spell.Cast("Thermal Grenade")
+                        ));
+            }
+        }
+    }
 }
--- trunk/DefaultCombat/Routines/Advanced/Gunslinger/Sharpshooter.cs
+++ trunk/DefaultCombat/Routines/Advanced/Gunslinger/Sharpshooter.cs
@@ -1,79 +1,89 @@
-﻿// Copyright (C) 2011-2015 Bossland GmbH
-// See the file LICENSE for the source code's detailed license
+﻿// Copyright (C) 2011-2015 Bossland GmbH// See the file LICENSE for the source code's detailed license
+
 
 using Buddy.BehaviorTree;
 using DefaultCombat.Core;
 using DefaultCombat.Helpers;
 
+
 namespace DefaultCombat.Routines
 {
-	internal class Sharpshooter : RotationBase
-	{
-		public override string Name
-		{
-			get { return "Gunslinger Sharpshooter"; }
-		}
+    internal class Sharpshooter : RotationBase
+    {
+        public override string Name
+        {
+            get { return "Gunslinger Sharpshooter"; }
+        }
 
-		public override Composite Buffs
-		{
-			get
-			{
-				return new PrioritySelector(
-					Spell.Buff("Lucky Shots")
-					);
-			}
-		}
 
-		public override Composite Cooldowns
-		{
-			get
-			{
-				return new LockSelector(
-					Spell.Buff("Escape"),
-					Spell.Buff("Burst Volley"),
-					Spell.Buff("Defense Screen", ret => Me.HealthPercent <= 70),
-					Spell.Buff("Dodge", ret => Me.HealthPercent <= 30),
-					Spell.Buff("Cool Head", ret => Me.EnergyPercent <= 50),
-					Spell.Buff("Smuggler's Luck"),
-					Spell.Buff("Illegal Mods")
-					);
-			}
-		}
+        public override Composite Buffs
+        {
+            get
+            {
+                return new PrioritySelector(
+                    Spell.Buff("Lucky Shots")
+                    );
+            }
+        }
 
-		public override Composite SingleTarget
-		{
-			get
-			{
-				return new LockSelector(
-					Spell.Cast("Flurry of Bolts", ret => Me.EnergyPercent < 60),
 
-					//Movement
-					CombatMovement.CloseDistance(Distance.Ranged),
+        public override Composite Cooldowns
+        {
+            get
+            {
+                return new PrioritySelector(
+                    Spell.Buff("Escape"),
+                    Spell.Buff("Burst Volley"),
+                    Spell.Buff("Defense Screen", ret => Me.HealthPercent <= 70),
+                    Spell.Buff("Dodge", ret => Me.HealthPercent <= 30),
+                    Spell.Buff("Cool Head", ret => Me.EnergyPercent <= 50),
+                    Spell.Buff("Smuggler's Luck"),
+                    Spell.Buff("Illegal Mods")
+                    );
+            }
+        }
 
-					//Rotation
-					Spell.Buff("Crouch", ret => !Me.IsInCover()),
-					Spell.Cast("Sabotage Charge", ret => Me.HasBuff("Burst Volley")),
-					Spell.Cast("Trickshot"),
-					Spell.Cast("Penetrating Rounds", ret => Me.IsInCover()),
-					Spell.DoT("Vital Shot", "Bleeding (Vital Shot)"),
-					Spell.Cast("Aimed Shot", ret => Me.IsInCover() && Me.BuffCount("Charged Aim") == 2),
-					Spell.Cast("Quickdraw", ret => Me.CurrentTarget.HealthPercent <= 30),
-					Spell.Cast("Charged Burst", ret => Me.IsInCover())
-					);
-			}
-		}
 
-		public override Composite AreaOfEffect
-		{
-			get
-			{
-				return new Decorator(ret => Targeting.ShouldAoe,
-					new LockSelector(
-						Spell.CastOnGround("XS Freighter Flyby"),
-						Spell.Cast("Thermal Grenade"),
-						Spell.CastOnGround("Sweeping Gunfire")
-						));
-			}
-		}
-	}
+        public override Composite SingleTarget
+        {
+            get
+            {
+                return new PrioritySelector(
+                    //Movement
+                    CombatMovement.CloseDistance(Distance.Ranged),
+
+                    //Low Energy
+                    new Decorator(ret => Me.EnergyPercent < 60,
+                        new PrioritySelector(
+                            Spell.Cast("Flurry of Bolts")
+                            )),
+
+
+
+                    //Rotation
+                    Spell.Cast("Trickshot"),
+                    Spell.Cast("Penetrating Rounds", ret => Me.Level >= 26),
+                    Spell.Cast("Speed Shot", ret => Me.Level < 26),
+                    Spell.DoT("Vital Shot", "Vital Shot"),
+                    Spell.Cast("Aimed Shot", ret => Me.BuffCount("Charged Aim") == 2),
+                    Spell.Cast("Quickdraw", ret => Me.CurrentTarget.HealthPercent <= 30),
+                    Spell.Cast("Charged Burst")
+                    );
+            }
+        }
+
+
+        public override Composite AreaOfEffect
+        {
+            get
+            {
+                return new Decorator(ret => Targeting.ShouldAoe,
+                    new PrioritySelector(
+                        Spell.CastOnGround("XS Freighter Flyby"),
+                        Spell.Cast("Thermal Grenade"),
+                        Spell.CastOnGround("Sweeping Gunfire")
+                        ));
+            }
+        }
+    }
 }
