0f3849d42954d9d8eb1e0365fd4e4f4f984c151c
Markeeen <Djahide2@gmail.com>
2016-01-03 11:34:46 +0100

    Update AdvancedPrototype.cs
    Added shoulder cannon and casts cooldowns on gold or greater.

--- trunk/DefaultCombat/Routines/Advanced/PowerTech/AdvancedPrototype.cs
+++ trunk/DefaultCombat/Routines/Advanced/PowerTech/AdvancedPrototype.cs
@@ -1,9 +1,11 @@
-﻿// Copyright (C) 2011-2015 Bossland GmbH
+// Copyright (C) 2011-2015 Bossland GmbH
 // See the file LICENSE for the source code's detailed license
+
 
 using Buddy.BehaviorTree;
 using DefaultCombat.Core;
 using DefaultCombat.Helpers;
+
 
 namespace DefaultCombat.Routines
 {
@@ -13,6 +15,7 @@ namespace DefaultCombat.Routines
 		{
 			get { return "PowerTech Advanced Prototype"; }
 		}
+
 
 		public override Composite Buffs
 		{
@@ -25,12 +28,16 @@ namespace DefaultCombat.Routines
 			}
 		}
 
+
 		public override Composite Cooldowns
 		{
 			get
 			{
-				return new LockSelector(
+				return new PrioritySelector(
 					Spell.Buff("Determination"),
+					Spell.Buff("Shoulder Cannon"),
+					Spell.Buff("Thermal Sensor Override", ret => Me.InCombat && Me.CurrentTarget.BossOrGreater()),
+					Spell.Buff("Explosive Fuel", ret => Me.InCombat && Me.CurrentTarget.BossOrGreater()),
 					Spell.Buff("Vent Heat", ret => Me.ResourcePercent() >= 50),
 					Spell.Buff("Energy Shield", ret => Me.HealthPercent <= 60),
 					Spell.Buff("Kolto Overload", ret => Me.HealthPercent <= 30)
@@ -38,18 +45,24 @@ namespace DefaultCombat.Routines
 			}
 		}
 
+
 		public override Composite SingleTarget
 		{
 			get
 			{
-				return new LockSelector(
+				return new PrioritySelector(
 					//Movement
 					CombatMovement.CloseDistance(Distance.Melee),
-					new Decorator(ret => Me.ResourcePercent() > 40,
-						new LockSelector(
+					
+					//Low Energy
+					new Decorator(ret => Me.ResourcePercent() < 50,
+						new PrioritySelector(
 							Spell.Cast("Rail Shot", ret => Me.HasBuff("Prototype Particle Accelerator")),
 							Spell.Cast("Rapid Shots")
 							)),
+							
+					//Rotation
+					Spell.Cast("Shoulder Cannon", ret => Me.HasBuff("Shoulder Cannon") && Me.CurrentTarget.BossOrGreater()),
 					Spell.Cast("Quell", ret => Me.CurrentTarget.IsCasting && !DefaultCombat.MovementDisabled),
 					Spell.Cast("Energy Burst", ret => Me.BuffCount("Energy Lode") == 4),
 					Spell.Cast("Rail Shot",
@@ -63,18 +76,19 @@ namespace DefaultCombat.Routines
 			}
 		}
 
+
 		public override Composite AreaOfEffect
 		{
 			get
 			{
-				return new LockSelector(
+				return new PrioritySelector(
 					new Decorator(ret => Targeting.ShouldAoe,
-						new LockSelector(
+						new PrioritySelector(
 							Spell.CastOnGround("Death from Above"),
 							Spell.Cast("Explosive Dart", ret => Me.CurrentTarget.HasDebuff("Bleeding (Retractable Blade)"))
 							)),
 					new Decorator(ret => Targeting.ShouldPbaoe,
-						new LockSelector(
+						new PrioritySelector(
 							Spell.Cast("Flame Thrower"),
 							Spell.Cast("Flame Sweep"))
 						));
