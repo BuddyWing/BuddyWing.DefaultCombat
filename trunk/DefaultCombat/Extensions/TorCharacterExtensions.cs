3fa404304e42b4d5deb4034e22b8e9da96c7e865
aevitas <m4rtijn@gmail.com>
2018-02-28 23:03:31 +0100

    Add ShouldDispel extension method

---
+++ trunk/DefaultCombat/Extensions/TorCharacterExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Buddy.Swtor.Objects;
+
+namespace DefaultCombat.Extensions
+{
+    public static class TorCharacterExtensions
+    {
+        public static bool ShouldDispel(this TorCharacter target, string debuffName)
+        {
+            if (target == null)
+                return false;
+
+            return target.HasDebuff(debuffName);
+        }
+    }
+}
